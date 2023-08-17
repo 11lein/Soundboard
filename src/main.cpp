faec6179e1b4370fecf60e419fa897a184757b9d
r11lein <ronny@11lein.de>
2023-08-17 18:09:55 +0200

    Merge branch 'dfplayer-fast'

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,6 @@
 
 #include "BluetoothSerial.h"
 
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-
-BluetoothSerial SerialBT;
-
 const byte ROWS = 5; // five rows
 const byte COLS = 5; // five columns
 char keys[ROWS][COLS] = {
